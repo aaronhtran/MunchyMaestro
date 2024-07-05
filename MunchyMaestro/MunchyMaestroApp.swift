6050e81b78bd7903d463e07067719bf56158fadc
Aaron Tran <aaron8tran@gmail.com>
2024-07-05 09:06:28 -0500

    Initial Commit

---
+++ MunchyMaestro/MunchyMaestroApp.swift
@@ -0,0 +1,17 @@
+//
+//  MunchyMaestroApp.swift
+//  MunchyMaestro
+//
+//  Created by Aaron Tran on 7/5/24.
+//
+
+import SwiftUI
+
+@main
+struct MunchyMaestroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
